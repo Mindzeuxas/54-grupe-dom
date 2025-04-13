90e10f139058813bba17eb3ed87441ed183652ac
mindaugas.samsonas <mindaugas.samsonas@studentas.bit.lt>
2025-04-13 16:22:17 +0300

    ups

--- js/components/header.js
+++ js/components/header.js
@@ -30,7 +30,7 @@ export function header() {
 
     const HTML = `
         <header class="main-header">
-            <img class="logo" src="./img/logo.png" alt="Logo">
+            <img class="logo" src="${base}/img/logo.png" alt="Logo">
             <nav class="main-nav">${linksHTML}</nav>
         </header>`;
 
